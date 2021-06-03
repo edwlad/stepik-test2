1befdeff0c625861094968dd5121e69102554433
edwlad <80802093+edwlad@users.noreply.github.com>
2021-06-03 11:33:13 +0700

    Update README and test_items.py

--- test_items.py
+++ test_items.py
@@ -13,8 +13,8 @@ links = [
 def test_find_add_to_basket_button(browser, link):
     browser.get(link)
     curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    if 'fr' in curr_language:
+        time.sleep(30)
     find_buttons = browser.find_elements_by_css_selector('#add_to_basket_form .btn-add-to-basket')
     assert len(find_buttons) > 0, 'Не найдена кнопка добавления в корзину'
     assert len(find_buttons) < 2, 'Кнопка добавления в корзину не уникальна'
-    if 'fr' in curr_language:
-        time.sleep(30)
